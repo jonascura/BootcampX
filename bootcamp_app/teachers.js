79ad0b6bd95d6c8136cedac1abdf172f60f86908
Jonas Cura <jonasm.cura@gmail.com.com>
2023-07-27 20:08:56 -0600

    Sanitize students.js and teachers.js

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -2,22 +2,26 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'vagrant',
-  password: '123',
+  password: 'labber',
   host: 'localhost',
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+const queryString =
+`SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-});
+WHERE cohorts.name = $1
+ORDER BY teacher;`;
+
+const cohortName = process.argv[2] || 'JUL02';
+const value = [cohortName];
+
+pool.query(queryString, value)
+  .then((res) => {
+    res.rows.forEach((row) => {
+      console.log(`${row.cohort}: ${row.teacher}`)
+    })
+  });
